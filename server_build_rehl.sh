85eb5784f761d8a2390f374d6e213b059c8ab5a8
Billy Dean <billy@deanhome.net>
2023-02-06 17:35:45 -0600

    Update server_build_rehl.sh

--- server_build_rehl.sh
+++ server_build_rehl.sh
@@ -2,21 +2,10 @@
 
 sudo yum update -y
 
-sudo yum -y module enable php:8.1
 
-sudo yum module install -y php:8.1/common
-# sudo yum install -y php php-cli php-xml php-gd php-pdo
 sudo yum install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo yum -y install wget
-HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
-
-sudo systemctl start php-fpm
-sudo systemctl enable php-fpm
-
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 sudo yum -y install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaDB-backup MariaDB-common
@@ -24,14 +13,31 @@ sudo yum -y install MariaDB-server galera-4 MariaDB-client MariaDB-shared MariaD
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
+
+sudo yum -y module enable php:8.1
+
+sudo yum module install -y php:8.1/common
+# sudo yum install -y php php-cli php-xml php-gd php-pdo
+
+sudo systemctl start php-fpm
+sudo systemctl enable php-fpm
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo yum -y install wget
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+
+
 sudo yum -y install vim
 sudo yum -y install git
 sudo yum -y install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo chown ec2-user:nginx /var/www/html -R
-echo "hello world" > index.html
-php -v >> index.html
+# echo "hello world" > index.html
+# php -v >> index.html
 
 ### from new /var/www/html/ directory
 ### composer create-project drupal/recommended-project:10.0.3 .
